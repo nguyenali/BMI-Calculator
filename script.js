1e999ff5b572b335eb8eba2a867a47a1009cb5c9
Ali Nguyen <alin57717@gmail.com>
2021-03-28 22:48:28 -0700

    create script constants

---
+++ script.js
@@ -0,0 +1,5 @@
+const BMIT_HEADS = document.querySelectorAll('.bmi-head');
+const BMI_USC = document.getElementById('bmi-usc');
+const BMI_SI = document.getElementById('bmi-si');
+const CALC_BTN = document.getElementById('calc-btn');
+const CLR_BTN = document.getElementById('clr-btn');
