8c5b54de2d3dfe182c86ed9467f17f9304a8e3de
Ali Nguyen <alin57717@gmail.com>
2021-03-28 22:56:13 -0700

    add toggle script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BMIT_HEADS = document.querySelectorAll('.bmi-head');
+const BMI_HEADS = document.querySelectorAll('.bmi-head');
 const BMI_USC = document.getElementById('bmi-usc');
 const BMI_SI = document.getElementById('bmi-si');
 const CALC_BTN = document.getElementById('calc-btn');
@@ -10,3 +10,16 @@ window.addEventListener('DOMContentLoaded', () => {
     BMI_USC.classList.add('show-bmi');
     activeForm = "bmi-usc";
 });
+
+// bmi calculation form toggle
+BMI_HEADS.forEach(bmiHead => {
+    bmiHead.addEventListener('click', () =>{
+        if(bmiHead.id === "bmi-si-head") {
+            removeActiveClass();
+            bmiHead.classList.add('active-head');
+            BMI_USC.classList,remove('show-bmi');
+            BMI_SI.add('show-bmi');
+            activeForm = "bmi-si";
+        }
+    })
+})
