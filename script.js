8d3363adbbf8df9be93a2ec7723708fa326d018b
Ali Nguyen <alin57717@gmail.com>
2021-03-28 22:50:20 -0700

    create event listeners

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const BMI_USC = document.getElementById('bmi-usc');
 const BMI_SI = document.getElementById('bmi-si');
 const CALC_BTN = document.getElementById('calc-btn');
 const CLR_BTN = document.getElementById('clr-btn');
+
+//event listeners
+
+window.addEventListener('DOMContentLoaded', () => {
+    BMI_USC.classList.add('show-bmi');
+    activeForm = "bmi-usc";
+});
