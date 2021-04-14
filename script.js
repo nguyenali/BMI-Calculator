b6da26e6737cdbdf95841dce51a24d036a51f347
Ali Nguyen <alin57717@gmail.com>
2021-04-13 22:26:02 -0700

    get input values

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ window.addEventListener('DOMContentLoaded', () => {
     activeForm = "bmi-usc";
 });
 
+
+CALC_BTN.addEventListener('click', performBMICalc);
+
 // bmi calculation form toggle
 BMI_HEADS.forEach(bmiHead => {
     bmiHead.addEventListener('click', () =>{
@@ -31,3 +34,32 @@ function removeActiveClass(){
         bmiHead.classList.remove('active-head');
     });
 }
+
+//main bmi calculation function
+
+function performBMICalc(){
+    
+}
+
+// get input values
+function getUserInput(){
+    let status;
+    // get input values from us units
+    if(activeForm === "bmi-usc"){
+        let age = document.getElementById('age1').value,
+        gender = document.querySelector('#bmi-usc input[name = "gender"]:checked').value,
+        heightFeet = document.getElementById('feet').value,
+        heightInches = document.getElementById('inches').value,
+        weightPounds = document.getElementById('pounds').value;
+        
+        status = checkInputStatus([age, heightFeet, heightInches, weightPounds]);
+
+        if(status == true){
+            return calculateBMI({
+                gender,
+                age, 
+                height: parseFloat(heightFeet) * 12 + parseFloat(heightInches),
+                weight: parseFloat(weightPounds)
+            });
+        }
+    }
